c68a7a9d3da9023324005e0e3fdd5b6a24f4ab21
ParthArora <partharora.india@gmail.com>
2021-02-12 21:05:17 +0530

    Header Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'relatas-task';
+export class AppComponent{
+  constructor(private http: HttpClient){
+    // userList;
+  }
+
+  ngOnInit(): void {
+    console.log("p")
+    this.http.get("https://api.github.com/users")
+    .subscribe(users => {
+      console.log(users)
+      // this.userList = users
+    });
+  }
+
 }
